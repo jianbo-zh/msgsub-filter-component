8951e16366db3553008e81ee70705b0cf97390f0
tipine <tipine@163.com>
2020-03-27 20:05:23 +0800

    test

---
+++ cmd/filtercmpt/hello.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":    "kafka:9092",
+		"group.id":             "",
+		"default.topic.config": kafka.ConfigMap{"auto.offset.reset": "earliest"}})
+
+	if err != nil {
+		panic(err)
+	}
+
+	run := true
+
+	err = consumer.SubscribeTopics([]string{"msgsub-filter"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for run == true {
+		ev := consumer.Poll(0)
+		switch e := ev.(type) {
+		case *kafka.Message: // application-specific processing
+			fmt.Printf("%% Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			run = false
+		default:
+			fmt.Printf("Ignored %v\n", e)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module msgsub/filtercmpt
+
+go 1.14
+
+require github.com/confluentinc/confluent-kafka-go v1.3.0
